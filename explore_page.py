4808b7b298c46c1235c01113d61f7f5311dfe390
mounal <mounalahmira@gmail.com>
2024-11-11 19:08:03 +0100

    load and cach the data

--- explore_page.py
+++ explore_page.py
@@ -26,3 +26,23 @@ def clean_education(x):
     if 'Professional degree' in x:
         return 'Professional degree'
     return 'Less than a Bachelors'
+@st.cache
+def load_data():
+    df = pd.read_csv('dataset/survey_results_public.csv')
+    df = df[["Country", "EdLevel", "YearsCodePro", "Employment", "ConvertedComp"]]
+    df = df.rename({'ConvertedComp': 'Salary'}, axis=1)
+    df = df[df["ConvertedComp"].notnull()]
+    df = df.dropna()
+    df = df[df["Employment"] == "Employed full-time"]
+    df = df.drop("Employment", axis=1)
+
+    country_map = shorten_categories(df.Country.value_counts(), 400)
+    df['Country'] = df['Country'].map(country_map)
+    df = df[df['Salary'] <= 250000]
+    df = df[df['Salary'] >= 10000]
+    df = df[df['Country'] != "Other"]
+
+    df['YearsCodePro'] = df['YearsCodePro'].apply(clean_experience)
+    df['EdLevel'] = df['EdLevel'].apply(clean_education)
+
+df = load_data()
