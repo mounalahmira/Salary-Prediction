9d5a5752f457ee8a5b39033533e155d93c81b11f
mounal <mounalahmira@gmail.com>
2024-11-11 19:19:58 +0100

    add 'show_explore_page' function

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 from predict_page import show_predict_page
+from explore_page import show_explore_page
 
-st.sidebar.selectbox("Explore Or Predict", ("Explore","Predict"))
-show_predict_page()
+page = st.sidebar.selectbox("Explore Or Predict", ("Explore","Predict"))
+if page == "Explore":
+    show_explore_page()
+else:
+    show_predict_page()
--- explore_page.py
+++ explore_page.py
@@ -30,7 +30,6 @@ def clean_education(x):
 def load_data():
     df = pd.read_csv('dataset/survey_results_public.csv')
     df = df[["Country", "EdLevel", "YearsCodePro", "Employment", "ConvertedComp"]]
-    df = df.rename({'ConvertedComp': 'Salary'}, axis=1)
     df = df[df["ConvertedComp"].notnull()]
     df = df.dropna()
     df = df[df["Employment"] == "Employed full-time"]
@@ -38,11 +37,28 @@ def load_data():
 
     country_map = shorten_categories(df.Country.value_counts(), 400)
     df['Country'] = df['Country'].map(country_map)
-    df = df[df['Salary'] <= 250000]
-    df = df[df['Salary'] >= 10000]
+    df = df[df['ConvertedComp'] <= 250000]
+    df = df[df['ConvertedComp'] >= 10000]
     df = df[df['Country'] != "Other"]
 
     df['YearsCodePro'] = df['YearsCodePro'].apply(clean_experience)
     df['EdLevel'] = df['EdLevel'].apply(clean_education)
+    df = df.rename({'ConvertedComp': 'Salary'}, axis=1)
+    return df
+
 
 df = load_data()
+
+def show_explore_page():
+    st.title("Explore Software Engineer Salaries")
+
+    st.write("""Stack Overflow Developer Survey 2020""")
+
+    data = df['Country'].value_counts()
+
+    fig1,ax1 = plt.subplots()
+    ax1.pie(data,labels=data.index,autopct='%1.1f',shadow=True,startangle=90)
+    ax1.axis('equal')
+
+    st.write("""#### Number of data from different countries""")
+    st.pyplot(fig1)
