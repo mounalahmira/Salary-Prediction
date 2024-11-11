6796cfadcefcff89b8ccc640048d6208b276bf93
mounal <mounalahmira@gmail.com>
2024-11-11 18:53:41 +0100

    creat the show_predict_page function and add some streamlit components

--- predict_page.py
+++ predict_page.py
@@ -18,3 +18,44 @@ def show_predict_page():
     st.title('Software Developer Salary Prediction')
 
     st.write("""### We need some information to predict the salary of the""")
+
+
+    countries = (
+            "United States",
+            "India",
+            "United Kingdom",
+            "Germany",
+            "Canada",
+            "Brazil",
+            "France",
+            "Spain",
+            "Australia",
+            "Netherlands",
+            "Poland",
+            "Italy",
+            "Russian Federation",
+            "Sweden",
+        )
+    education = (
+        "Less than a Bachelors",
+        "Bachelor’s degree",
+        "Master’s degree",
+        "Post grad",
+    )
+
+    country = st.selectbox("Country", countries)
+    education = st.selectbox("Education Level", education)
+
+    experience = st.slider("Years fo Experience",0,50,3)
+
+    ok = st.button("Calculate Salary")
+    if ok:
+        X = np.array([[country, education, experience]])
+        X[:, 0] = le_country.transform(X[:, 0])
+        X[:, 1] = le_education.transform(X[:, 1])
+        X = X.astype(float)
+
+        salary = regressor.predict(X)
+        st.subheader(f"The estimated salary is ${salary[0]:.2f}")
+
+
